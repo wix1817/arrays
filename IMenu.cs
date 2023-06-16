0ac03334783a8f5222c13eff4de18f48da37edd3
Aliaksei Pustavy <wix1817@gmail.com>
2023-06-16 23:10:03 +0200

    Update ApplicationMenu.cs, IMenu.cs, and 5 more files...

---
+++ IMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    internal interface IMenu
+    {
+        static abstract void Start();
+
+        static abstract void DrawRule(string ruleName);
+    }
+}
